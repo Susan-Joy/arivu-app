091e05e4a5cca41fed8298c583c65380b854fbd5
Sahil Shefeek <sahilms345@gmail.com>
2024-07-27 07:28:43 +0530

    added community fn

--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -1,10 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class CommunityPage extends StatelessWidget {
-  const CommunityPage({super.key});
+class CommunityPage extends StatefulWidget {
+  const CommunityPage({Key? key}) : super(key: key);
+
+  @override
+  _CommunityPageState createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> {
+  List videos = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchVideos();
+  }
+
+  Future<void> fetchVideos() async {
+    final response =
+        await http.get(Uri.parse('http://34.69.190.172/api/youtube-videos/'));
+    if (response.statusCode == 200) {
+      setState(() {
+        videos = json.decode(response.body);
+        isLoading = false;
+      });
+    } else {
+      // Handle server error
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Community',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: videos.length,
+              itemBuilder: (context, index) {
+                final video = videos[index];
+                final videoId =
+                    YoutubePlayer.convertUrlToId(video['video_url']);
+                if (videoId == null) {
+                  // Skip invalid videos
+                  return const SizedBox.shrink();
+                }
+                return Card(
+                  margin: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          video['title'],
+                          style: const TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(video['description']),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: AspectRatio(
+                          aspectRatio: 16 / 9,
+                          child: VideoPlayer(videoUrl: video['video_url']),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
+
+class VideoPlayer extends StatefulWidget {
+  final String videoUrl;
+
+  const VideoPlayer({Key? key, required this.videoUrl}) : super(key: key);
+
+  @override
+  _VideoPlayerState createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  late YoutubePlayerController _controller;
+  String? videoId;
+
+  @override
+  void initState() {
+    super.initState();
+    videoId = YoutubePlayer.convertUrlToId(widget.videoUrl);
+    if (videoId != null) {
+      _controller = YoutubePlayerController(
+        initialVideoId: videoId!,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (videoId == null) {
+      return const Center(
+        child: Text('Invalid YouTube video URL'),
+      );
+    }
+    return YoutubePlayer(
+      controller: _controller,
+      showVideoProgressIndicator: true,
+    );
   }
 }
